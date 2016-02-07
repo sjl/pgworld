fdb98c892c324a78868cd7e331675a606997a960
Steve Losh <steve@stevelosh.com>
2016-02-07 15:09:08 +0000

    Add a noiser for Terrain objects too

---
+++ Assets/Editor/TerrainNoiseEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainNoise))]
+public class TerrainNoiseEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		TerrainNoise noiser = (TerrainNoise)target;
+		if (GUILayout.Button("Run")) {
+			noiser.Run();
+		}
+	}
+}
---
+++ Assets/Scripts/Noise/TerrainNoise.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Terrain))]
+[RequireComponent(typeof(Noise))]
+public class TerrainNoise : MonoBehaviour {
+	Terrain terrain;
+	Noise noise;
+
+	void Start() {
+		terrain = GetComponent<Terrain>();
+		noise = GetComponent<Noise>();
+	}
+
+	public void Run() {
+		noise.Init();
+
+		int width = terrain.terrainData.heightmapWidth;
+		int height = terrain.terrainData.heightmapHeight;
+
+		float[,] heightmap = new float[height, width];
+		for (int z = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				heightmap[z, x] = noise.Get(x, z);
+			}
+		}
+
+		terrain.terrainData.SetHeights(0, 0, heightmap);
+	}
+
+}
