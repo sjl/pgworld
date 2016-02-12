ce2a4c9a8f54f8d28750e48a5c36c82492b8aeea
einargizz <einarog05@ru.is>
2016-02-12 13:34:59 +0000

    Created a Terrain folder
    When you make a new terrain, give it a unique name and put it in the
    Terrain folder.
    
    This will limit conflict errors, since Unity names all new terrain "New
    Terrain" and puts it the core asset folder.

---
+++ Assets/caustics/CausticsProjector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [ExecuteInEditMode]
+    class CausticsProjector : MonoBehaviour
+    {
+        public int FramesPerSecond = 30;
+
+        private new Renderer renderer;
+
+        public Texture[] causticsTextures = null;
+
+        void Start()
+        {
+            renderer = GetComponent<Renderer>();
+        }
+
+        void Update()
+        {
+
+            if (causticsTextures != null && causticsTextures.Length >= 1)
+            {
+                int causticsIndex = (int)(Time.time * FramesPerSecond) % causticsTextures.Length;
+                renderer.sharedMaterial.SetTexture("_EmissionMap", causticsTextures[causticsIndex]);
+            }
+
+            Vector3 LightDirection = new Vector3(10, 10, 5);
+            var lightMatrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.LookRotation(LightDirection, new Vector3(LightDirection.z, LightDirection.x, LightDirection.y)), Vector3.one);
+            renderer.sharedMaterial.SetMatrix("_CausticsLightOrientation", lightMatrix);
+            print(lightMatrix);
+        }
+    }
+}
