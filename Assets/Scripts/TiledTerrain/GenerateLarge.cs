8110ebd4974663d530329fa99af76b3c41ecd814
einargizz <einarog05@ru.is>
2016-02-26 13:16:53 +0000

    Tiled terrain script - Experiment
    Created a folder TiledTerrain and LargeTerrain

---
+++ Assets/Scripts/TiledTerrain/GenerateLarge.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateLarge : MonoBehaviour {
+
+	public GameObject plane;
+	public GameObject player;
+	public int planeSize = 10;
+	public int halfTilesX = 10;
+	public int halfTilesZ = 10;
+
+	Vector3 startPos;
+
+	Hashtable tiles = new Hashtable();
+
+	void Start() {
+		this.gameObject.transform.position = Vector3.zero;
+		startPos = Vector3.zero;
+
+		float updateTime = Time.realtimeSinceStartup;
+
+		for (int x = -halfTilesX; x < halfTilesX; x++) {
+			for (int z = -halfTilesZ; z < halfTilesZ; z++) {
+				Vector3 pos = new Vector3((x * planeSize+startPos.x), 0, (z * planeSize+startPos.z));
+				GameObject t = (GameObject) Instantiate(plane, pos, Quaternion.identity);
+
+				string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+				t.name = tilename;
+				Tile tile = new Tile(t, updateTime);
+				tiles.Add(tilename, tile);
+			}
+		}
+	}
+}
--- Assets/Scripts/Underwater/UnderWater.cs
+++ Assets/Scripts/Underwater/UnderWater.cs
@@ -7,23 +7,16 @@ public class UnderWater : MonoBehaviour {
 
     public float underwaterLevel = 0;
 	public float deepLevel = 50;
-	private bool isUnderwater, canSwim;
+	private bool isUnderwater;
 	public Color normalColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
 	public Color deepColor = new Color(0.0f, 0.0f, 0.0f, 0.8f);
 	public Color underwaterColor = new Color(0, 0.4f, 0.7f, 0.1f);
 	private Vector3 above, below, swimUp, sink;
 	public GameObject waterplane = null;
-	public GameObject controller = null;
-//	private ConstantForce force;
-	private Rigidbody body;
 
 	void Start() {
 		above = new Vector3(0.0f, 0.0f, 0.0f);
 		below = new Vector3(180.0f, 0.0f, 0.0f);
-		swimUp = new Vector3(0.0f, 10.0f, 0.0f);
-		sink = new Vector3(0.0f, -2.0f, 0.0f);
-		body = controller.GetComponent<Rigidbody>();
-//		force = controller.GetComponent<ConstantForce>();
 	}
 
     void Update() {
@@ -34,14 +27,6 @@ public class UnderWater : MonoBehaviour {
 		} else if (!isUnderwater) {
 			SetNormal();
 		}
-
-//		if (canSwim) {
-//			if (Input.GetKey(KeyCode.E)) {
-//				force.relativeForce = swimUp;
-//			} else {
-//				force.relativeForce = sink;
-//			}
-//		}
 	}
 
 	void SetUnderwater() {
@@ -53,16 +38,10 @@ public class UnderWater : MonoBehaviour {
 		RenderSettings.fogColor = Color.Lerp(underwaterColor, deepColor, percentDepth);
 		RenderSettings.fogDensity = Mathf.Lerp(0.005f, 0.5f, percentDepth);
 		waterplane.transform.localEulerAngles = below;
-		body.useGravity = false;
-		canSwim = true;
-		//body.isKinematic = true;
     }
     void SetNormal() {
     	RenderSettings.fogColor = normalColor;
 		RenderSettings.fogDensity = 0.000000008f;
 		waterplane.transform.localEulerAngles = above;
-		body.useGravity = true;
-		canSwim = false;
-		//body.isKinematic = false;
 	}
 }
